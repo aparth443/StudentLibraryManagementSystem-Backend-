aedf5d6294a49d25aa7c28218d74b057b15d8e30
aparth443 <aparth443@gmail.com>
2023-02-17 11:27:13 +0530

    get author using dto

--- src/main/java/com/example/Library_Management_System/Services/AuthorService.java
+++ src/main/java/com/example/Library_Management_System/Services/AuthorService.java
@@ -1,10 +1,16 @@
 package com.example.Library_Management_System.Services;
 
 import com.example.Library_Management_System.DTOs.AuthorEntryDto;
+import com.example.Library_Management_System.DTOs.AuthorResponseDto;
+import com.example.Library_Management_System.DTOs.BookResponseDto;
 import com.example.Library_Management_System.Models.Author;
+import com.example.Library_Management_System.Models.Book;
 import com.example.Library_Management_System.Repositories.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AuthorService {
@@ -23,5 +29,30 @@ public class AuthorService {
         
         authorRepository.save(author);
         return "Author added successfully";
+    }
+
+    public AuthorResponseDto getAuthor(int authorId){
+        Author author = authorRepository.findById(authorId).get();
+
+        AuthorResponseDto authorResponseDto = new AuthorResponseDto();
+
+        List<Book> bookList = author.getBooksWritten();
+        List<BookResponseDto> bookResponseDtoList = new ArrayList<>();
+
+        for(Book b: bookList){
+            BookResponseDto bookResponseDto = new BookResponseDto();
+            bookResponseDto.setName(b.getName());
+            bookResponseDto.setPages(b.getPages());
+            bookResponseDto.setGenre(b.getGenre());
+
+            bookResponseDtoList.add(bookResponseDto);
+        }
+
+        authorResponseDto.setName(author.getName());
+        authorResponseDto.setAge(author.getAge());
+        authorResponseDto.setBooksWritten(bookResponseDtoList);
+        authorResponseDto.setRating(author.getRating());
+        authorResponseDto.setCountry(author.getCountry());
+        return authorResponseDto;
     }
 }
